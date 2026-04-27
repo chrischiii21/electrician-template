0b9f623d09900d940ce1cd8a8033ed30024e592c
chrischiii21 <christymontejo2003@gmail.com>
2026-04-27 14:43:28 +0800

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Replace with your actual domain
   vite: {
     plugins: [tailwindcss()]
   }
